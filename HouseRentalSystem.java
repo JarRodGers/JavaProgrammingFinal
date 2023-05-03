c8856e622e3440763da2c6d46d167fbfb65998f8
JarRodGers <117948248+JarRodGers@users.noreply.github.com>
2023-05-03 09:44:58 -0400

    Create HouseRentalSystem.java
    Added program headed and set up class frameworks

---
+++ HouseRentalSystem.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////// 
+// Student name: Jarrod Rodgers                                                //    
+// Date due: 05/03/2023                                                        // 
+// Date submitted: 05/03/2023                                                  //
+// Program name: House Rental System                                           //
+// Program description: This program keeps track of houses and guests. Houses  //
+// and guests can be registered, rented, or deleted. Availablity for houses    //
+// will also be available. The program is built on a basic CRUD framework.     //
+/////////////////////////////////////////////////////////////////////////////////
+
+public class HouseRentalSystem {
+    public static void main(String[] args) {
+
+    }
+}
+
+class House {
+
+}
+
+class Guest {
+    
+}
