9d230f8a32ec63fbc036bc3a618b130610e1d434
JarRodGers <117948248+JarRodGers@users.noreply.github.com>
2023-05-03 10:51:51 -0400

    Calendar sidebar
    added a method to create a calendar year in a 2d array; will be used to verify dates

--- HouseRentalSystem.java
+++ HouseRentalSystem.java
@@ -35,11 +35,34 @@ class House {
         ownerName = newOwnerName;
         address = newAddress;
         costPerNight = newCostPerNight;
+
     }
 
     // This method adds a House object to the house list
     void registerHouse(House property, ArrayList<House> list) {
         list.add(property);
+    }
+
+    // This method creates a calendar year in a 2 dimensional array; February is assumed to have 28 days
+    int[][] createCalendar() {
+        // create 2d array with 12 rows
+        int[][] calendar = new int[12][];
+
+        // initialize each row with number of days in corresponding month
+        calendar[0] = new int[31];
+        calendar[1] = new int[28];
+        calendar[2] = new int[31];
+        calendar[3] = new int[30];
+        calendar[4] = new int[31];
+        calendar[5] = new int[30];
+        calendar[6] = new int[31];
+        calendar[7] = new int[31];
+        calendar[8] = new int[30];
+        calendar[9] = new int[31];
+        calendar[10] = new int[30];
+        calendar[11] = new int[31];
+
+        return calendar;
     }
 }
 
